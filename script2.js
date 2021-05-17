14a388a450a93cfe5c8d5899831ee43f9e8e4095
emmanuel1982 <emmanuel.eichler@gmail.com>
2021-05-17 18:52:17 -0400

    modified code

--- script2.js
+++ script2.js
@@ -45,51 +45,59 @@ addButton.addEventListener('click', function () {
         const position = todoArray.indexOf(listItem.innerHTML);
         todoArray.splice(position, 1);
     })
-
-    // Your sort event listener is inside your add event listener. Let's remove it to the outside because it is independant of the items. You want to create the sort event listener only once and not each time the add button gets hit. 
-    sort.addEventListener('change', () => {
-        const sortValue = document.getElementById("sort");
-        if (sortValue.options[sortValue.selectedIndex].text == 'Ascending') {
-            // You want to remove the todo list (ul) instead of the item as this will only remove the text not the buttons
-            // And you want to so the same in descending as well
-            listItem.remove();
-            asc = todoArray.sort();
-
-            for (let i = 0; i < asc.length; i++) {
-                // 'asc' is a variable that has your sorted array. It's not a component so you won't be able to do any DOM manipulation to it. 
-                // You have deleted the ul at this point so you will need to recreate everything - see line 18 where you create those elements. 
-                // You'll also need to recreate the ul which we created in the html because we just deleted it above. Then append everything to the ul
-                // So at this point you may have noticed that you are basically writing the same code several times i.e. creating those elements in the add event listener, in sort scending and sort descending. You want to create a function that does that and just call the function each time. 
-                // ** Hint: In the function, you'll be passing in an array as that is the only thing that changes each time you create the elements. 
+});
+// Your sort event listener is inside your add event listener. Let's remove it to the outside because it is independant of the items. 
+// You want to create the sort event listener only once and not each time the add button gets hit. - done
+sort.addEventListener('change', () => {
+    const sortValue = document.getElementById("sort");
+    if (sortValue.options[sortValue.selectedIndex].text == 'Ascending') {
+        // You want to remove the todo list (ul) instead of the item as this will only remove the text not the buttons
+        // And you want to so the same in descending as well - done
+        listItem = document.getElementById('todoList');
+        listItem.remove();
+        asc = todoArray.sort();
+        
+        for (let i = 0; i < asc.length; i++) {
+            // 'asc' is a variable that has your sorted array. It's not a component so you won't be able to do any DOM manipulation to it. 
+            // You have deleted the ul at this point so you will need to recreate everything - see line 18 where you create those elements. 
+            // You'll also need to recreate the ul which we created in the html because we just deleted it above. Then append everything to the ul
+            // So at this point you may have noticed that you are basically writing the same code several times i.e. creating those elements in the add 
+            //event listener, in sort ascending and sort descending. You want to create a function that does that and just call the function each time. 
+            // ** Hint: In the function, you'll be passing in an array as that is the only thing that changes each time you create the elements. 
+            
+            let sortedTodoList = (asc) => {
                 asc[i].innerHTML = listItem;
+                listItemDiv = document.getElementById('outputContainer');
                 listItemDiv.appendChild(listItem);
-                listItemDiv.appendChild(listItemButton);
-                listItemDiv.appendChild(removeButton);
-                todoList.appendChild(listItemDiv);
             }
-
-
-        } else if (sortValue.options[sortValue.selectedIndex].text == 'Descending') {
-            desc = todoArray.sort().reverse();
-
-            for (let i = 0; i < asc.length; i++) {
-                desc[i].innerHTML = listItem;
-                listItemDiv.appendChild(listItem);
-                listItemDiv.appendChild(listItemButton);
-                listItemDiv.appendChild(removeButton);
-                todoList.appendChild(listItemDiv);
-            }
+            sortedTodoList(asc);
         }
 
-        /*
-        if (sortValue.options[1].label == 'Ascending') {
-            const asc = todoArray.sort((a, b) => a - b);
-            console.log(asc);
-            //asc.forEach(item => listItem.innerHTML);
-        } else {
-            const desc = todoArray.sort((a, b) => b - a);
-            console.log(desc);
-            //desc.forEach(item => listItem.innerHTML);
-        }*/
-    })
+
+    } else if (sortValue.options[sortValue.selectedIndex].text == 'Descending') {
+        
+        listItem = document.getElementById('todoList');
+        listItem.remove();
+        desc = todoArray.sort().reverse();
+
+        for (let i = 0; i < desc.length; i++) {
+            let sortedTodoList = (desc) => {
+                desc[i].innerHTML = listItem;
+                listItemDiv = document.getElementById('outputContainer');
+                listItemDiv.appendChild(listItem);
+            }
+            sortedTodoList(desc);
+        }
+    }
+
+    /*
+    if (sortValue.options[1].label == 'Ascending') {
+        const asc = todoArray.sort((a, b) => a - b);
+        console.log(asc);
+        //asc.forEach(item => listItem.innerHTML);
+    } else {
+        const desc = todoArray.sort((a, b) => b - a);
+        console.log(desc);
+        //desc.forEach(item => listItem.innerHTML);
+    }*/
 });
